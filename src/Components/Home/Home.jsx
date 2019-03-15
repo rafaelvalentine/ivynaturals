be424c2baeaa7d06f0abefbe6c0094e51e912a11
rafaelvalentine <ozichukwuezike@hotmail.com>
2019-03-15 15:43:58 +0100

    second commit

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Jumbotron, Button, Card, Container, Row, Col } from 'react-bootstrap'
 import { Helmet } from 'react-helmet'
 import bannerImage from '../../Img/model-two.jpg'
+import Products from './Products/Products'
 import './Home.css'
 
 const ValueList = (props) => {
@@ -20,6 +21,23 @@ const ValueList = (props) => {
     </Col>
   )
 }
+
+// const BenefitList = (props) => {
+//   const { text, title, number, img } = props
+//   return (
+
+//     <Col xs={12}>
+//       <div className=''>
+//         <img src={img} alt={img} />
+//         <div className=''>
+//           <div className='number'>{number}</div>
+//           <div className='title'>{title}</div>
+//           <div className='text'>{text}</div>
+//         </div>
+//       </div>
+//     </Col>
+//   )
+// }
 export default class Home extends Component {
  static defaultProps = {
  values:[
@@ -41,12 +59,32 @@ export default class Home extends Component {
     title:'Designed for you',
     text:'Custom-made for you by our a in-house Dermatologist.'
    }
- ]
+ ],
+ benefits:[
+  {
+   img:'assets/img/intelligence.svg',
+   title:'Enrich your skin',
+   text:'Let of our brand of naturally derived products rejuvinate you'
+  },
+  {
+    img:'assets/img/fingerprints.svg',
+    title:'Enrich your skin',
+    text:'Let of our brand of naturally derived products rejuvinate you'
+   },
+   {
+    img:'assets/img/badge.svg',
+    title:'Enrich your skin',
+    text:'Let of our brand of naturally derived products rejuvinate you'
+   }
+]
   }
   render () {
     const values = this.props.values.map((value, index)=>(
         <ValueList key={index} {...value}/>
     ))
+    const benefits = this.props.benefits.map((value, index)=>(
+      <ValueList key={index} {...value}/>
+  ))
     return (
       <div>
         <Helmet>
@@ -92,6 +130,74 @@ export default class Home extends Component {
                 {values}
               </Row>
             </Container>
+          </div>
+        </section>
+
+
+          {/*
+         ==========================================================================
+          Products
+         ==========================================================================
+        */}
+
+        <section id='products' className='products'>
+        <div className='header'>
+          <h2>
+            Products
+          </h2>
+          <hr/>
+        </div>
+          <div className='products-content'>
+              <Products/>
+          </div>
+        </section>
+
+         {/*
+         ==========================================================================
+          Products
+         ==========================================================================
+        */}
+
+        <section id='benefits' className='benefits'>
+        <div className='header'>
+          <h2>
+            Ivy's Benefits
+          </h2>
+          <hr/>
+        </div>
+          <div className='benefits-content'>
+              <Row>
+                  {benefits}
+              </Row>
+          </div>
+        </section>
+
+        {/*
+         ==========================================================================
+          Team
+         ==========================================================================
+        */}
+
+<section id='team' className='team'>
+        <div className='header'>
+          <h2>
+            meet the Crème de la Crème
+          </h2>
+          <hr/>
+        </div>
+          <div className='team-content'>
+                  <Card style={{ width: '18rem' }}>
+                    <Card.Img className='team-img' variant="top" src="assets/img/ivy_owner.jpg" />
+                    <Card.Body>
+                      <Card.Title>Barr. Ifeoma Mmamelu</Card.Title>
+                      <Card.Text className='team-text'>
+                      Before founding Ivy Natural, Barr. Ifeoma was a fully time law attorney. She has a Masters in Property Law from Stanford.
+                      </Card.Text>
+                      <Card.Text className='team-text'>
+                      She is also a mother and health enthusiast. 
+                      </Card.Text>
+                    </Card.Body>
+                  </Card>
           </div>
         </section>
       </div>
---
+++ src/Components/Home/Products/Products.jsx
@@ -0,0 +1,185 @@
+import React, { Component } from 'react'
+import { Button, Card, Container, Row, Col, Tabs, Tab } from 'react-bootstrap'
+import './Products.css'
+
+const ProductCard = (props) => {
+  const {img, title, text, amount} = props
+  return (
+    <Col xs={12} md={6} lg={4}>
+      <Card className='product-card'>
+        <Card.Img variant='top' src={img}  alt={`image of ${title}`}/>
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+          <Card.Text>
+              {text}
+          </Card.Text>
+          <Card.Text className='naira'>
+              &#8358; {amount}
+          </Card.Text>
+          {/* <Button className='product-card_button' variant='primary'>Go somewhere</Button> */}
+        </Card.Body>
+      </Card>
+    </Col>
+  )
+}
+
+export default class Products extends Component {
+  static defaultProps = {
+    lotions:[
+      {
+        img:'assets/img/lotion.png',
+        title:'Ivy Lotion',
+        text:'Let our all natural lotion hydrate your skin',
+        amount:'1,135'
+      },
+      {
+        img:'assets/img/lotion.png',
+        title:'Ivy Lotion',
+        text:'Let our all natural lotion hydrate your skin',
+        amount:'1,135'
+      },
+      {
+        img:'assets/img/lotion.png',
+        title:'Ivy Lotion',
+        text:'Let our all natural lotion hydrate your skin',
+        amount:'1,135'
+      },
+      {
+        img:'assets/img/lotion.png',
+        title:'Ivy Lotion',
+        text:'Let our all natural lotion hydrate your skin',
+        amount:'1,135'
+      },
+      {
+        img:'assets/img/lotion.png',
+        title:'Ivy Lotion',
+        text:'Let our all natural lotion hydrate your skin',
+        amount:'1,135'
+      },
+      {
+        img:'assets/img/lotion.png',
+        title:'Ivy Lotion',
+        text:'Let our all natural lotion hydrate your skin',
+        amount:'1,135'
+      }
+    ],
+    creams:[
+      {
+        img:'assets/img/cream.jpg',
+        title:'Ivy Cream',
+        text:'Let our all natural cream refresh your skin',
+        amount:'2,375'
+      },
+      {
+        img:'assets/img/cream.jpg',
+        title:'Ivy Cream',
+        text:'Let our all natural cream refresh your skin',
+        amount:'2,375'
+      },
+      {
+        img:'assets/img/cream.jpg',
+        title:'Ivy Cream',
+        text:'Let our all natural cream refresh your skin',
+        amount:'2,375'
+      },
+      {
+        img:'assets/img/cream.jpg',
+        title:'Ivy Cream',
+        text:'Let our all natural cream refresh your skin',
+        amount:'2,375'
+      },
+      {
+        img:'assets/img/cream.jpg',
+        title:'Ivy Cream',
+        text:'Let our all natural cream refresh your skin',
+        amount:'2,375'
+      },
+      {
+        img:'assets/img/cream.jpg',
+        title:'Ivy Cream',
+        text:'Let our all natural cream refresh your skin',
+        amount:'2,375'
+      }
+    ],
+    soaps:[
+      {
+        img:'assets/img/soap.jpg',
+        title:'Ivy Soap',
+        text:'Let our all natural soap recreate your skin',
+        amount:'1,775'
+      },
+      {
+        img:'assets/img/soap.jpg',
+        title:'Ivy Soap',
+        text:'Let our all natural soap recreate your skin',
+        amount:'1,775'
+      },
+      {
+        img:'assets/img/soap.jpg',
+        title:'Ivy Soap',
+        text:'Let our all natural soap recreate your skin',
+        amount:'1,775'
+      },
+      {
+        img:'assets/img/soap.jpg',
+        title:'Ivy Soap',
+        text:'Let our all natural soap recreate your skin',
+        amount:'1,775'
+      },
+      {
+        img:'assets/img/soap.jpg',
+        title:'Ivy Soap',
+        text:'Let our all natural soap recreate your skin',
+        amount:'1,775'
+      },
+      {
+        img:'assets/img/soap.jpg',
+        title:'Ivy Soap',
+        text:'Let our all natural soap recreate your skin',
+        amount:'1,775'
+      }
+    ]
+  }
+  render () {
+    const lotionCard = this.props.lotions.map((product, index)=>(
+      <ProductCard key={index} {...product}/>
+    ))
+    const creamCard = this.props.creams.map((product, index)=>(
+      <ProductCard key={index} {...product}/>
+    ))
+    const soapCard = this.props.soaps.map((product, index)=>(
+      <ProductCard key={index} {...product}/>
+    ))
+    return (
+      <div>
+        <Container>
+          <Tabs defaultActiveKey='lotion' id='product-tab' mountOnEnter='false'>
+            <Tab mountOnEnter='true' eventKey='lotion' title='Lotions'>
+            <Row>
+            {lotionCard}
+            </Row>
+              
+            </Tab>
+            <Tab eventKey='cream' title='Creams'>
+            <Row>
+            {creamCard}
+            </Row>
+            </Tab>
+            <Tab eventKey='soap' title='Soaps'>
+            <Row>
+            {soapCard}
+            </Row>
+            </Tab>
+            {/* <Tab eventKey='profile3' title='Profile'>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </p>
+            </Tab>
+            <Tab eventKey='profile4' title='Profile'>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </p>
+            </Tab>
+            <Tab eventKey='contact' title='Contact' disabled /> */}
+          </Tabs>
+        </Container>
+      </div>
+    )
+  }
+}
